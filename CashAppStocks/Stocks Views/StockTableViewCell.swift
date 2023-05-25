7e52ea3afa48d2c1b1741e14cfdeddf5d7e1440a
John_Ugonabo <chuxugonabo@gmail.com>
2023-05-25 16:11:07 -0700

    Created and added a custom table view, along with the UI and display for the Stock Item cell

--- CashAppStocks/Stocks Views/StockTableViewCell.swift
+++ CashAppStocks/Stocks Views/StockTableViewCell.swift
@@ -9,15 +9,39 @@ import UIKit
 
 class StockTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var stockTickerLabelView: UILabel!
+    @IBOutlet weak var stockNameLabelView: UILabel!
+    @IBOutlet weak var stockQuantityLabelView: UILabel!
+    @IBOutlet weak var stockCurrentPriceLabelView: UILabel!
+    @IBOutlet weak var stockTotalAmountLabelView: UILabel!
+    
+    static let cellIdentifer = "StockTableViewCell"
+
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    static func nib() ->  UINib {
+        return UINib(nibName: cellIdentifer, bundle: nil)
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
     
+    func configure(stock: Stock) {
+        stockTickerLabelView.text = stock.ticker
+        stockNameLabelView.text = stock.name
+        stockQuantityLabelView.text = "\(stock.quantity)"
+        stockCurrentPriceLabelView.text = "@ \(stock.currentPriceCents)"
+        
+        var currentPrice = stock.currentPriceCents / 100
+        var totalAmount = (stock.quantity ?? 0) * currentPrice
+        
+        stockTotalAmountLabelView.text = "\(totalAmount)"
+    }
 }
